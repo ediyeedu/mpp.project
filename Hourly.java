b8d90c93521d4d49e557ec26a42a2c18ccf7b6a5
ediyedu <ediyeedu@gmail.com>
2023-11-06 23:45:07 -0600

    initial commit

---
+++ Hourly.java
@@ -0,0 +1,18 @@
+package AssignmentLab4;
+
+
+public class Hourly extends AEmployee{
+   double hourlyWage;
+    double hoursPerWeek;
+
+    public Hourly(String name,double hourlyWage,double hoursPerWeek) {
+        super(name);
+        this.hoursPerWeek = hoursPerWeek;
+        this.hourlyWage = hourlyWage;
+    }
+
+    @Override
+    double calcGrossPay(DateRange dateRange) {
+        return hourlyWage * hoursPerWeek * 4;
+    }
+}
---
+++ Order.java
@@ -0,0 +1,42 @@
+package AssignmentLab4;
+
+import java.time.LocalDateTime;
+
+public class Order {
+    private int orderNo;
+    private LocalDateTime orderDate;
+    private double orderAmount;
+
+    public int getOrderNo() {
+        return orderNo;
+    }
+
+    public void setOrderNo(int orderNo) {
+        this.orderNo = orderNo;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public double getOrderAmount() {
+        return orderAmount;
+    }
+
+    public void setOrderAmount(double orderAmount) {
+        this.orderAmount = orderAmount;
+    }
+
+    public Order(int orderNo, LocalDateTime orderDate, double orderAmount) {
+        this.orderNo = orderNo;
+        this.orderDate = orderDate;
+        this.orderAmount = orderAmount;
+    }
+}
+
+
+
